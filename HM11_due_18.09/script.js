8524c191b6c50221a93b9caa43b192bff2252691
Daniyar <baddaniar@gmail.com>
2022-09-19 00:11:33 +0600

    hw2

--- HM11_due_18.09/script.js
+++ HM11_due_18.09/script.js
@@ -1,3 +1,30 @@
 //Шахматная доска
+function chessboard(){
+    for(let i = 0; i < 4 ; i++){
+        console.log("|#  #  #  # |")
+        console.log("| #  #  #  #|")
+    }
+}
+//chessboard()
+
 //FizzBuzz
+function FizzBuzz(){
+    for(let j = 1; j < 100; j++){
+        if(j % 5 === 0 && j % 3 === 0){
+            console.log('FizzBuzz')
+        }else if(j % 5 === 0 && j % 3 !== 0){
+            console.log("Buzz")
+        }else if(j % 3 === 0){
+            console.log('Fizz')
+        }else{
+            console.log(j)
+        }
+    }
+}
+//FizzBuzz()
+
 //Палидрос
+let test = 'asddsa'
+function palidrome(string){
+    return string.split('').reverse().join('') == string
+}
