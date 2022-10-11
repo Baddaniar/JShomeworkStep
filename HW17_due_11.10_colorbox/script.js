db92da7a0c7c73ef88af6f08628b5b7c7944e89f
Daniyar <baddaniar@gmail.com>
2022-10-12 01:37:58 +0600

    somethinf wrong

--- HW17_due_11.10_colorbox/script.js
+++ HW17_due_11.10_colorbox/script.js
@@ -6,24 +6,99 @@ let code = document.querySelector("#Code")
 
 let save = document.querySelector("#saveButton")
 let allcolors = document.querySelector(".allcolors")
-let colornumber = 1
-save.addEventListener("click", function(){
 
+let colorerror = document.querySelector("#colorError")
+let codeerror = document.querySelector("#codeError")
+
+save.addEventListener("click", function(){
     let mycolor = {
         color: color.value,
         type: type.value,
         code: code.value
     }
-    //console.log(mycolor)
-    localStorage.setItem(colornumber, JSON.stringify(mycolor))
-
-    //console.log(colornumber)
-    let box = document.createElement(`div`)
-    box.className = "colorBox"
-    box.innerHTML = `
-    <p>${JSON.parse(localStorage.getItem(colornumber)).color}</p>
-    <p>${JSON.parse(localStorage.getItem(colornumber)).type}</p>
-    <p>${JSON.parse(localStorage.getItem(colornumber)).code}</p>`
-    allcolors.append(box)
-    colornumber++
+    let colors = []
+    if(checkData(mycolor)){//Тутсделаем проверку на заполнение типа check data 
+        if(localStorage.getItem("colors") === null){
+            colors.push(mycolor);
+            localStorage.setItem("colors", JSON.stringify(colors));
+            createbox()
+            colorerror.innerText = ""
+            codeerror.innerText = ""
+        }else{
+            colors = JSON.parse(localStorage.getItem("colors"));
+            colors.push(mycolor);
+            localStorage.setItem("colors", JSON.stringify(colors));
+            colorerror.innerText = ""
+            codeerror.innerText = ""
+            createbox()
+        }
+    }
 })
+if(localStorage.getItem("colors") !== null){
+    createbox()
+}
+
+
+function createbox(){
+    let collorsgrid = document.querySelector(`.allcolors`)
+    collorsgrid.innerHTML = ""
+    let allcolors = JSON.parse(localStorage.getItem("colors"));
+    allcolors.forEach((el) => {
+        let colorcode = ''
+        if(el.type == "RGBA"){
+            colorcode = "rgba"
+        }else if(el.type == "RGB"){
+            colorcode = "rgb"
+        }else{
+            colorcode = "hex"
+        }
+        collorsgrid.innerHTML += `
+        <div class="colorBox" style="background-color: ${colorcode}(${el.code});">
+        <div class="innerbox">
+        <p>${el.color}</p>
+        <p>${el.type}</p>
+        <p>${el.code}</p>
+        </div></div>`
+    })
+}
+
+
+function checkData(mycolor) {
+    if(!validatecolorname(mycolor.color)){
+        colorerror.innerText = "color can contain only letters"
+        return false
+    }else if(!validatetype(mycolor.code)){
+        codeerror.innerText = "Wrong pattern"
+        return false
+    }else{
+        return true
+    }
+}
+
+const validatecolorname = (color) => {
+    return String(color)
+        .match(
+            /[a-zA-Z]/
+        )};
+//Добить всю проверку!
+const validatetype = (type) =>{
+    if(type.value = "RGB"){
+        return String(type)
+        .match(
+            /^(\d{1,3}), (\d{1,3}), (\d{1,3})$/
+    )}else if(type.value = "RGBA"){
+        return String(type)
+        .match(
+            /^(\d{1,3}), (\d{1,3}), (\d{1,3}), \d\.\d$/)
+    }else{
+        return String(type)
+        .match(
+            /^#[0-9a-f]{3,6}$/i)
+    }
+}
+
+
+
+
+
+
