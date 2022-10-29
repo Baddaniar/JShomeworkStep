08f7d79037df6502e3fd5403292caf057e972fdb
unknown <baddaniar@gmail.com>
2022-10-29 14:59:38 +0600

    hw drag

--- HW28_due_28.10_video_drag/script.js
+++ HW28_due_28.10_video_drag/script.js
@@ -1,5 +1,4 @@
 //1
-
 let video = document.querySelector("video")
 
 video.addEventListener("mouseover", () => {
@@ -8,3 +7,18 @@ video.addEventListener("mouseover", () => {
 video.addEventListener("mouseout", () => {
     video.play()
 })
+
+//2
+function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.append(document.getElementById(data));
+  }
